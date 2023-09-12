6ff06ef97cf0cd4def1dfb03134fdab49b3f779a
Mikael Frykholm <mifr@sunet.se>
2023-09-12 14:58:32 +0200

    -y

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ else
     git clone --branch "${UPSTREAM_VERSION=main}" --depth 1 https://github.com/swedenconnect/bankid-saml-idp.git "${GIT_DIR}"
     pushd "${GIT_DIR}"
 fi
-apt install -f maven
+apt install -y maven
 mvn clean install
 DOCKER_IMAGE="docker://debian:bookworm-slim-java" 
 mvn -f bankid-idp/bankid-idp-backend jib:dockerBuild  -Djib.from.image="${DOCKER_IMAGE}"
