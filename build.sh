d77ca48c3894a11ef8bc36426f47a14aba661e5f
Mikael Frykholm <mifr@sunet.se>
2024-09-02 14:54:42 +0200

    Build multiple versions.

--- build.sh
+++ build.sh
@@ -3,23 +3,26 @@ set -e
 export DOCKER_REPO=docker.sunet.se
 docker build -t debian:bookworm-slim-java build-pre-image
 
-GIT_DIR="build_bankid-saml-idp"
+for version in ${UPSTREAM_VERSIONS}; do
+    GIT_DIR="build_bankid-saml-idp"
 
-if [ -d "${GIT_DIR}" ]; then
-    rm -rf "${GIT_DIR}"
-fi
-echo "Using ${UPSTREAM_VERSION}."
-git clone --branch "${UPSTREAM_VERSION=main}" --depth 1 https://github.com/swedenconnect/bankid-saml-idp.git "${GIT_DIR}"
-pushd "${GIT_DIR}"
+    if [ -d "${GIT_DIR}" ]; then
+        rm -rf "${GIT_DIR}"
+    fi
+    echo "Using ${version}."
+    git clone --branch "${version=main}" --depth 1 https://github.com/swedenconnect/bankid-saml-idp.git "${GIT_DIR}"
+    pushd "${GIT_DIR}"
 
-apt-get update
-apt-get install -y openjdk-17-jdk-headless
-apt-get install -y maven
-mvn -version
-mvn clean install
+    apt-get update
+    apt-get install -y openjdk-17-jdk-headless
+    apt-get install -y maven
+    mvn -version
+    mvn clean install
 
-DOCKER_IMAGE="docker://debian:bookworm-slim-java" 
+    DOCKER_IMAGE="docker://debian:bookworm-slim-java" 
 
-#patch to use new test cert
-cp ../FPTestcert5_20240610.pem bankid-idp/src/main/resources/bankid-trust-test.crt
-mvn -f bankid-idp jib:dockerBuild@local -Djib.from.image="${DOCKER_IMAGE}" -Djib.to.image="${DOCKER_REPO}/bankid-saml-idp"
+    #patch to use new test cert
+    cp ../FPTestcert5_20240610.pem bankid-idp/src/main/resources/bankid-trust-test.crt
+    mvn -f bankid-idp jib:dockerBuild@local -Djib.from.image="${DOCKER_IMAGE}" -Djib.to.image="${DOCKER_REPO}/bankid-idp:${version}"
+    docker push "${DOCKER_REPO}/bankid-idp:${version}"
+done
